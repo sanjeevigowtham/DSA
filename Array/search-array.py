ed5f04d5393585c397e28e13aacf75e102c1e8a9
sanjeevigowtham <sanjeevigowtham23@gmail.com>
2026-02-14 13:39:47 +0530

    search an array

---
+++ Array/search-array.py
@@ -0,0 +1,9 @@
+# https://www.geeksforgeeks.org/problems/search-an-element-in-an-array-1587115621/1
+
+class Solution:
+    def search(self, arr, x):
+        # code here
+        for i in range (len(arr)):
+            if arr[i]==x:
+                return i
+        return -1
